b9c21fd09a8fb9ced1f5f277af1e3ecf3acbdd84
joaovam <amorimvictorjoao3@gmail.com>
2025-03-24 10:54:11 -0300

    adding programs generated using ChiGen default grammar

---
+++ ChiGen/10k_programs_per_k/1gram/jgproject/.tmp/.initCmds.tcl
@@ -0,0 +1,3 @@
+@puts {INFO: successfully checked out licenses "jasper_interactive" and "jasper_fao".}
+@puts {INFO: reading configuration file "/home/joaovam/.config/cadence/jasper.conf".}
+@get_message -clear
---
+++ ChiGen/10k_programs_per_k/1gram/jgproject/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,11 @@
+# ----------------------------------------
+# Jasper Version Info
+# tool      : Jasper 2023.12
+# platform  : Linux 6.5.0-28-generic
+# version   : 2023.12p002 64 bits
+# build date: 2024.02.21 12:57:35 UTC
+# ----------------------------------------
+# started   : 2025-03-21 08:52:03 -03
+# hostname  : britto.(none)
+# pid       : 58189
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:34879' '-nowindow' '-style' 'windows' '-data' 'AAAAxHicY2RgYLCp////PwMYMFcBCQEGHwZfhiAGVyDpzxAGpOGA8QGUYcPIgApAfCZUkcAGFJqBgRWmGaYEpEGLQZchkSEHCPMZyhniGUoZ8hiKgWQBEOYzFDGUMKQypADF/RmCgar5gKrzgOLxDOlANZlgUwCxrhUR' '-proj' '/home/joaovam/projects/chimera/chigen_programs/10k_programs_per_k/1gram/jgproject/sessionLogs/session_0' '-init' '-hidden' '/home/joaovam/projects/chimera/chigen_programs/10k_programs_per_k/1gram/jgproject/.tmp/.initCmds.tcl'
